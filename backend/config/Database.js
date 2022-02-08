243b0346623ceb2453c5015239437893b921bf0f
Suhailsaifi780 <suhails780@gmail.com>
2022-02-08 13:35:52 +0530

    aa

--- backend/config/Database.js
+++ backend/config/Database.js
@@ -1,8 +1,10 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('auth_db', 'root', 'root', {
-    host: "localhost",
+const db = new Sequelize('heroku_47b88148051d26c', 'ba0433346ecd98', '99b6f0a2', {
+    host: "us-cdbr-east-05.cleardb.net",
     dialect: "mysql"
 });
 
 export default db;
+
+
